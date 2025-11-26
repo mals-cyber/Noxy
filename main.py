c0c14f627019afc318f2100dccd8c3cd85de0691
malscyber <maloutlot@gmail.com>
2025-11-26 13:46:28 +0800

    cors policy updated

--- main.py
+++ main.py
@@ -26,6 +26,9 @@ app.add_middleware(
         "http://127.0.0.1:5173",
         "http://localhost:5164",
         "http://127.0.0.1:5164",
+
+        "http://20.106.176.126:5173",
+        "http://20.106.176.126",
     ],
     allow_credentials=True,
     allow_methods=["*"],
